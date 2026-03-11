5e244057aff3f2522f012a270214b71009ce9473
barous <bao@oven.bz>
2026-03-11 10:24:57 +0700

    feat: implement EnvConfigService for app config management

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { TaskGroupModel } from 'generated/prisma/models/TaskGroup';
 
 @Controller()
 export class AppController {
@@ -8,12 +7,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    const taskGroup: TaskGroupModel = {
-      id: 1,
-      name: 'Example Task Group',
-      tasks: [],
-    };
-    console.log('Task Group:', taskGroup);
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { EnvConfigService } from './config/envConfig.service';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, EnvConfigService],
+  exports: [EnvConfigService],
 })
 export class AppModule {}
