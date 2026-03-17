076eba1bb042d83cfa1c6807e35a13aa09ca0d71
barous <bao@oven.bz>
2026-03-17 17:15:50 +0700

    feat/implement-user-authentication: - rename to .env.example - delete unnessary EnvConfigModule

--- src/modules/token/token.module.ts
+++ src/modules/token/token.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TokenService } from './token.service';
-import { EnvConfigModule } from 'src/config/envConfig.module';
 
 @Module({
-  imports: [EnvConfigModule],
   providers: [TokenService],
   exports: [TokenService],
 })
